dc4dd195acd9e9a37b423db272665eb9a8882356
himansu8 <himansusekharsahu50@gmail.com>
2024-08-07 01:11:53 +0530

    category crud operation done in admin dashboard

--- client/src/pages/admin/CreateCategory.jsx
+++ client/src/pages/admin/CreateCategory.jsx
@@ -1,8 +1,127 @@
-import React from 'react'
-import Layout from '../../components/layout/Layout'
-import AdminSideBar from '../../components/layout/AdminSideBar'
+import React, { useEffect, useState } from 'react';
+import Layout from '../../components/layout/Layout';
+import AdminSideBar from '../../components/layout/AdminSideBar';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import CreateCategoryForm from '../../components/form/CreateCategoryForm';
+import { useAuth } from '../../context/auth';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import { Modal } from "antd";
+
 
 function CreateCategory() {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [auth] = useAuth();
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
+
+  // Handle Form Submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/category/create-category`,
+        { name },
+        {
+          headers: {
+            authorization: auth?.token,
+          },
+        });
+      if (data?.success) {
+        toast.success(`${name} is created`);
+        setName("");
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in input form");
+    }
+  };
+
+  // Get All Categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/category/get-category`);
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting categories");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  //update category
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `${process.env.REACT_APP_BASE_URL}/api/category/update-category/${selected._id}`,
+        { name: updatedName },
+        {
+          headers: {
+            authorization: auth?.token,
+          },
+        }
+      );
+      if (data?.success) {
+        toast.success(`${updatedName} is updated`);
+        setSelected(null);
+        setUpdatedName("");
+        setVisible(false);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // Handle Delete
+  const handleDelete = async (id) => {
+    confirmAlert({
+      title: 'Confirm to delete',
+      message: 'Are you sure you want to delete this category?',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: async () => {
+            try {
+              const { data } = await axios.delete(
+                `${process.env.REACT_APP_BASE_URL}/api/category/delete-category/${id}`,
+                {
+                  headers: {
+                    authorization: auth?.token,
+                  },
+                }
+              );
+              if (data.success) {
+                toast.success(`Category is deleted`);
+                getAllCategory();
+              } else {
+                toast.error(data.message);
+              }
+            } catch (error) {
+              toast.error("Something went wrong");
+            }
+          }
+        },
+        {
+          label: 'No',
+          onClick: () => toast.error("Deletion cancelled")
+        }
+      ]
+    });
+  };
+
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -11,12 +130,61 @@ function CreateCategory() {
             <AdminSideBar />
           </div>
           <div className="col-md-9">
-            <h1>Create Category</h1>
+            <h1>Manage Category</h1>
+            <div className="p-3 w-50">
+              <CreateCategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
+            </div>
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((c) => (
+                    <tr key={c._id}>
+                      <td>{c.slug}</td>
+                      <td>
+                        <button
+                          className="btn btn-primary ms-2"
+                          onClick={() => {
+                            setVisible(true);
+                            setUpdatedName(c.name);
+                            setSelected(c);
+                          }}
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="btn btn-danger ms-2"
+                          onClick={() => handleDelete(c._id)}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <Modal onCancel={() => setVisible(false)}
+              footer={null}
+              visible={visible}>
+              <CreateCategoryForm value={updatedName}
+                setValue={setUpdatedName}
+                handleSubmit={handleUpdate} />
+            </Modal>
           </div>
         </div>
       </div>
     </Layout>
-  )
+  );
 }
 
-export default CreateCategory
+export default CreateCategory;
